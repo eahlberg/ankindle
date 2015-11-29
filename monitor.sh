d0ebed41f1da02aec69a4e2519369be311759b0d
Eric Ahlberg <eahlberg@gmail.com>
2015-11-29 17:37:17 +0100

    Add check that creates CSV file upon Kindle connection

--- monitor.sh
+++ monitor.sh
@@ -1,15 +1,45 @@
 #!/bin/sh
 
+filename="history.log"
+maxsize=14500
+
 while true
 do
-  if ls /Volumes/ | grep Kindle
+  filesize=$(ls -l "$filename" | awk '{print $5}')
+
+  if ls /Volumes/ | grep -q Kindle
   then
-    echo "Kindle connected, creating CSV file"
+
+    if [ $filesize -ge $maxsize ]
+    then
+      echo "[$(date)] Kindle connected, creating CSV file" > history.log
+      echo "[$(date)] CSV created. Waiting for disconnect..." > history.log
+    else
+      echo "[$(date)] Kindle connected, creating CSV file" >> history.log
+      echo "[$(date)] CSV created. Waiting for disconnect..." >> history.log
+    fi
+
     python create_csv.py
-    echo "CSV created"
-    break
+
+    while true
+    do
+      if ! ls /Volumes/ | grep -q Kindle
+      then
+        break
+      else
+        :
+      sleep 5
+    fi
+    done
+
   else
-    echo "Kindle not connected"
+    if [ $filesize -ge $maxsize ]
+    then
+      echo "[$(date)] Kindle not connected" > history.log
+    else
+      echo "[$(date)] Kindle not connected" >> history.log
+    fi
+
   fi
 
   sleep 5
